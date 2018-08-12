65c1b75bb8d9237d92bf150f1044f745449d39b0
Nathan Heller <naheller@gmail.com>
2018-08-12 09:36:36 -0700

    header

--- src/views/pages/Event/index.js
+++ src/views/pages/Event/index.js
@@ -11,7 +11,6 @@ class EventPage extends Component {
     componentDidMount() {
         const eventId = get(this.props, 'computedMatch.params.id', '')
         this.props.getEventById(eventId);
-        console.log('EventPage props', this.props);
     }
 
     showTitleCard() {
@@ -23,20 +22,23 @@ class EventPage extends Component {
         const eventDescription = get(event, 'description', '')
 
         return (
-            <div className="jumbotron p-4">
-                <h1 className="display-4">{eventTitle}</h1>
-                <p className="lead">
-                    We are a private, 501(c)(3) nonprofit food bank dedicated to alleviating hunger by
-                    creating access to food.
-                </p>
-                <hr className="my-4"/>
-                <p>
-                    We accomplish this by soliciting, collecting, rescuing, growing, preparing and packaging food for
-                    distribution through a network of more than 150 partner agencies and distribution sites; through
-                    public awareness, education and community advocacy; and through programs designed to improve the
-                    ability of low-income individuals to maintain an adequate supply of wholesome, nutritious
-                    food.
-                </p>
+            <div className="row">
+                <div className="col-8">
+                    <div className="jumbotron p-4">
+                        <h1 className="display-4">{eventTitle}</h1>
+                        <p className="lead">
+                            We are a private, 501(c)(3) nonprofit food bank dedicated to alleviating hunger by
+                            creating access to food.
+                        </p>
+                        <hr className="my-4"/>
+                        <p>{eventDescription}</p>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="jumbotron p-4">
+                        <p>Are you going?</p>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -52,7 +54,6 @@ class EventPage extends Component {
 }
 
 const mapStateToProps = (state) => {
-    console.log('Event state', state);
     return ({
         selectedEvent: get(state, 'selectedEvent', {}),
         eventStatus: get(state, 'selectedEvent.status', 'NOT_STARTED')
